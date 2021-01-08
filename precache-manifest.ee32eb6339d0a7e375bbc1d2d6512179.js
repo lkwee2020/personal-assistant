916a8736542399311f0cc33e9dc3fcceaf682ff2
Lauren <lkwee@college.harvard.edu>
2021-01-08 09:30:26 -1000

    Updates

---
+++ precache-manifest.ee32eb6339d0a7e375bbc1d2d6512179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319cadce15bdccf0f091a777adb2fbb0",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "a2f7bab1c8e71c824e12",
+    "url": "/personal-assistant/static/js/2.d15a1176.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/personal-assistant/static/js/2.d15a1176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0e362e8f4ba99b1e90",
+    "url": "/personal-assistant/static/js/main.2e1d71eb.chunk.js"
+  },
+  {
+    "revision": "b8b8e24a3d2cb0a90037",
+    "url": "/personal-assistant/static/js/runtime-main.c67efb09.js"
+  }
+]);
